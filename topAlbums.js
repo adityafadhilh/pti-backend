a0d2d1ccc65aca5483396bb09b389039b11cffb2
Aditya Fadhil Herindro <af.herindro@gmail.com>
2023-12-18 19:45:17 +0700

    fixed top albums

--- topAlbums.js
+++ topAlbums.js
@@ -3,61 +3,61 @@ const topAlbumsData = [
         artistId: 1,
         artistName: "Kendrick Lamar",
         collectionName: "To Pimp a Butterfly ",
-        artworkUrl100: "https://is1-ssl.mzstatic.com/image/thumb/Music126/v4/97/1a/a3/971aa332-2c7d-947c-33ed-4635d41115a4/23UM1IM56239.rgb.jpg/316x316bf.webp"
+        artworkUrl100: "https://e.snmc.io/i/300/w/101eb2cc9a7b875ee26086c9f44d1213/8121875/Kendrick%20Lamar%20-%20To%20Pimp%20a%20Butterfly%2C%20Cover%20art.webp"
     },
     {
         artistId: 2,
         artistName: "Radiohead",
         collectionName: "OK Computer",
-        artworkUrl100: "https://e.snmc.io/i/300/w/cb41b0acd3d7c304e1d1052b615ea300/8862252/Radiohead%20-%20OK%20Computer%2C%20Cover%20art.webp"
+        artworkUrl100: "https://e.snmc.io/i/300/w/9aef6a39b68e6191d36c2b52be8e59f4/8862252/Radiohead%20-%20OK%20Computer%2C%20Cover%20art.webp"
     },
     {
         artistId: 3,
         artistName: "Pink Floyd",
         collectionName: "Wish You Were Here",
-        artworkUrl100: "https://e.snmc.io/i/300/w/4a254e6f1a9d64edae48659b98939b2e/4184635/Pink%20Floyd%20-%20Wish%20You%20Were%20Here%2C%20Cover%20art.webp"
+        artworkUrl100: "https://e.snmc.io/i/300/w/be9c2932e778e5101468fc16f5f80b7b/4184635/Pink%20Floyd%20-%20Wish%20You%20Were%20Here%2C%20Cover%20art.webp"
     },
     {
         artistId: 4,
         artistName: "Radiohead",
         collectionName: "In Rainbows",
-        artworkUrl100: "https://e.snmc.io/i/300/w/484b57ab2605dad8b546b441a71e3043/5914164/Radiohead%20-%20In%20Rainbows%2C%20Cover%20art.webp"
+        artworkUrl100: "https://e.snmc.io/i/300/w/182804fa5b1d40568ca4bcbbdc1fac9b/5914164/Radiohead%20-%20In%20Rainbows%2C%20Cover%20art.webp"
     },
     {
         artistId: 5,
-        artistName: "Madvillainy",
-        collectionName: "Madvillain",
-        artworkUrl100: "https://e.snmc.io/i/300/w/56d7cbc8e10866f5deaf8183045beffc/10532981/Madvillain%20-%20Madvillainy%2C%20Cover%20art.webp"
+        artistName: "Madvillain",
+        collectionName: "Madvillainy",
+        artworkUrl100: "https://e.snmc.io/i/300/w/da8fba8e35558481541cf8bc5c4bce68/10532981/Madvillain%20-%20Madvillainy%2C%20Cover%20art.webp"
     },
     {
         artistId: 6,
         artistName: "King Crimson",
         collectionName: "In the Court of the Crimson King",
-        artworkUrl100: "https://e.snmc.io/i/300/w/028715735d39f4d2250ed2a4acafdd6c/7999484/King%20Crimson%20-%20In%20the%20Court%20of%20the%20Crimson%20King%2C%20Cover%20art.webp"
+        artworkUrl100: "https://e.snmc.io/i/300/w/799f264b30a066608b02c0cfed04dddd/7999484/King%20Crimson%20-%20In%20the%20Court%20of%20the%20Crimson%20King%2C%20Cover%20art.webp"
     },
     {
         artistId: 7,
-        artistName: "Kid A",
-        collectionName: "Radiohead",
-        artworkUrl100: "https://e.snmc.io/i/300/w/32f09d438dd264262b4012893148aa61/8879684/Radiohead%20-%20Kid%20A%2C%20Cover%20art.webp"
+        artistName: "Radiohead",
+        collectionName: "Kid A",
+        artworkUrl100: "https://e.snmc.io/i/300/w/56c0bfced9cc5478a6e184d222c403c6/8879684/Radiohead%20-%20Kid%20A%2C%20Cover%20art.webp"
     },
     {
         artistId: 8,
-        artistName: "good kid, m.A.A.d city",
-        collectionName: "Kendrick Lamar",
-        artworkUrl100: "https://e.snmc.io/i/300/w/9686fb0ad9e1eb9475a3ad22b770ace9/11410589/Kendrick%20Lamar%20-%20good%20kid%2C%20m_A_A_d%20city%2C%20Cover%20art.webp"
+        artistName: "Kendrick Lamar",
+        collectionName: "good kid, m.A.A.d city",
+        artworkUrl100: "https://e.snmc.io/i/300/w/8d762ef4f50aace0fb7b2c16dba266e7/11410589/Kendrick%20Lamar%20-%20good%20kid%2C%20m_A_A_d%20city%2C%20Cover%20art.webp"
     },
     {
         artistId: 9,
         artistName: "My Bloody Valentine",
         collectionName: "Loveless",
-        artworkUrl100: "https://e.snmc.io/i/300/w/e2b1f17f208ef01174446bab527902e9/8080265/My%20Bloody%20Valentine%20-%20Loveless%2C%20Cover%20art.webp"
+        artworkUrl100: "https://e.snmc.io/i/300/w/8e44d13cb3d21638b64098d8a146e6ef/8080265/My%20Bloody%20Valentine%20-%20Loveless%2C%20Cover%20art.webp"
     },
     {
         artistId: 10,
         artistName: "Pink Floyd",
         collectionName: "The Dark Side of the Moon",
-        artworkUrl100: "https://e.snmc.io/i/300/w/59977e03acbdfd4c017f8f4fcff3458d/11510042/Pink%20Floyd%20-%20The%20Dark%20Side%20of%20the%20Moon%2C%20Cover%20art.webp"
+        artworkUrl100: "https://e.snmc.io/i/300/w/c7fef07097d8d259d71ec81308f55a7e/11510042/Pink%20Floyd%20-%20The%20Dark%20Side%20of%20the%20Moon%2C%20Cover%20art.webp"
     },
 ]
 
